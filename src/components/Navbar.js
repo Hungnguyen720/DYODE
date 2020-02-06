94cb674f52cade46f4c8a725e15cf7f81af994db
Hung Nguyen <hungnguyen720@gmail.com>
2020-02-06 07:41:59 -0800

    banner and nav bar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,25 +4,34 @@ class Navbar extends React.Component {
   render() {
     return (
       <nav className="nav-bar">
-        <div className="hamburger-text-wrapper">
-          <div className="hamburger">
-            <div class="line"></div>
-            <div class="line"></div>
-            <div class="line"></div>
+        <div class="nav-bar-row has-flex">
+          <div className="hamburger-text-wrapper col">
+            <div className="hamburger">
+              <div className="line"></div>
+              <div className="line"></div>
+              <div className="line"></div>
+            </div>
+          </div>
+          <div className="nav-col">
+            <span>Logo</span>
+          </div>
+          <div className="nav-col">
+            <ul className="nav-links">
+              <li>
+                <a href="/#">About</a>
+              </li>
+              <li>
+                <a href="/#">Contacts</a>
+              </li>
+              <li>
+                <a href="/#">Projects</a>
+              </li>
+            </ul>
+          </div>
+          <div className="nav-col">
+            <span>cart</span>
           </div>
         </div>
-        <span>Logo</span>
-        <ul className="nav-links">
-          <li>
-            <a href="/#">About</a>
-          </li>
-          <li>
-            <a href="/#">Contacts</a>
-          </li>
-          <li>
-            <a href="/#">Projects</a>
-          </li>
-        </ul>
       </nav>
     );
   }
